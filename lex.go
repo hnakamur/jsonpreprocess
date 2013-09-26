efad18847bf8a2d4c9c201033d3008240c95c980
Hiroaki Nakamura <hnakamur@gmail.com>
2013-09-26 21:52:24 +0900

    Add more itemTypes.

--- lex.go
+++ lex.go
@@ -3,6 +3,7 @@ package jsonpreprocess
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
@@ -23,17 +24,66 @@ func (i item) String() string {
 	case i.typ == itemError:
 		return i.val
 	}
-	return fmt.Sprintf("%q", i.val)
+	return fmt.Sprintf("%s:%q", i.typ, i.val)
 }
 
 type itemType int
 
+func (t itemType) String() string {
+	switch t {
+	case itemError:
+		return "error"
+	case itemString:
+		return "string"
+	case itemLeftBrace:
+		return "leftBrace"
+	case itemRightBrace:
+		return "rightBrace"
+	case itemLeftBracket:
+		return "leftBracket"
+	case itemRightBracket:
+		return "rightBracket"
+	case itemComma:
+		return "comma"
+	case itemColon:
+		return "colon"
+	case itemNumber:
+		return "number"
+	case itemTrue:
+		return "true"
+	case itemFalse:
+		return "false"
+	case itemNull:
+		return "null"
+	case itemWhitespace:
+		return "whitespace"
+	case itemIdentifier:
+		return "identifier"
+	case itemBlockComment:
+		return "blockComment"
+	case itemLineComment:
+		return "lineComment"
+	default:
+		panic(errors.New("unexpected itemType"))
+	}
+}
+
 const (
 	itemError itemType = iota
 	itemString
-	itemText
 	itemBlockComment
 	itemLineComment
+	itemLeftBrace
+	itemRightBrace
+	itemLeftBracket
+	itemRightBracket
+	itemComma
+	itemColon
+	itemNumber
+	itemTrue
+	itemFalse
+	itemNull
+	itemIdentifier
 	itemWhitespace
 	itemEOF
 )
@@ -127,6 +177,12 @@ func (l *lexer) accept(valid string) bool {
 	return false
 }
 
+func (l *lexer) acceptRun(valid string) {
+	for strings.IndexRune(valid, l.peek()) >= 0 {
+		l.next()
+	}
+}
+
 func (l *lexer) errorf(format string, args ...interface{}) stateFn {
 	l.items <- item{itemError, l.start, fmt.Sprintf(format, args...)}
 	return nil
@@ -158,43 +214,58 @@ const (
 )
 
 func lexText(l *lexer) stateFn {
+Loop:
 	for {
 		if l.hasPrefix(doubleQuote) {
-			if l.pos > l.start {
-				l.emit(itemText)
-			}
 			return lexString
 		} else if l.hasPrefix(lineComment) {
-			if l.pos > l.start {
-				l.emit(itemText)
-			}
 			return lexLineComment
 		} else if l.hasPrefix(leftComment) {
-			if l.pos > l.start {
-				l.emit(itemText)
-			}
 			return lexBlockComment
 		}
 
 		r := l.peek()
-		if unicode.IsSpace(r) {
-			if l.pos > l.start {
-				l.emit(itemText)
+		switch r {
+		case ':':
+			l.next()
+			l.emit(itemColon)
+		case ',':
+			l.next()
+			l.emit(itemComma)
+		case '{':
+			l.next()
+			l.emit(itemLeftBrace)
+		case '}':
+			l.next()
+			l.emit(itemRightBrace)
+		case '[':
+			l.next()
+			l.emit(itemLeftBracket)
+		case ']':
+			l.next()
+			l.emit(itemRightBracket)
+		default:
+			if unicode.IsSpace(r) {
+				return lexWhitespace
+			} else if r == eof {
+				l.next()
+				break Loop
+			} else if strings.IndexRune("0123456789-", l.peek()) >= 0 {
+				return lexNumber
+			} else {
+				return lexIdentifier
 			}
-			return lexWhitespace
-		} else if r == eof {
-			l.next()
-			break
-		} else {
-			l.next()
 		}
-	}
-	if l.pos > l.start {
-		l.emit(itemText)
 	}
 	l.emit(itemEOF)
 	return nil
 }
+
+const (
+	hexdigit  = "0123456789ABDEFabcdef"
+	digit     = "0123456789"
+	digit1To9 = "123456789"
+)
 
 func lexString(l *lexer) stateFn {
 	l.next()
@@ -208,7 +279,7 @@ func lexString(l *lexer) stateFn {
 				break
 			} else if r := l.next(); r == 'u' {
 				for i := 0; i < 4; i++ {
-					if !l.accept("0123456789ABCDEFabcdef") {
+					if !l.accept(hexdigit) {
 						return l.errorf("expected 4 hexadecimal digits")
 					}
 				}
@@ -221,6 +292,67 @@ func lexString(l *lexer) stateFn {
 			return l.errorf("unclosed string")
 		}
 	}
+}
+
+func lexNumber(l *lexer) stateFn {
+	l.accept("-")
+	if l.accept(digit1To9) {
+		l.acceptRun(digit)
+	} else if !l.accept("0") {
+		return l.errorf("bad digit for number")
+	}
+	if l.accept(".") {
+		l.acceptRun(digit)
+	}
+	if l.accept("eE") {
+		l.accept("+-")
+		if !l.accept(digit) {
+			return l.errorf("digit expected for number exponent")
+		}
+		l.acceptRun(digit)
+	}
+	l.emit(itemNumber)
+	return lexText
+}
+
+func lexIdentifier(l *lexer) stateFn {
+	r := l.peek()
+	if unicode.IsLetter(r) || r == '$' || r == '_' {
+		// do nothing
+	} else if r == '\\' {
+		if !l.accept("u") {
+			return l.errorf("'u' for unicode escape sequence expected")
+		}
+		for i := 0; i < 4; i++ {
+			if !l.accept(hexdigit) {
+				return l.errorf("expected 4 hexadecimal digits for unicode escape sequence")
+			}
+		}
+	} else {
+		return l.errorf("identifier expected")
+	}
+
+	for r = l.peek(); isIdentifierPart(r); {
+		l.next()
+		r = l.peek()
+	}
+
+	text := l.buffer.String()
+	if text == "true" {
+		l.emit(itemTrue)
+	} else if text == "false" {
+		l.emit(itemFalse)
+	} else if text == "null" {
+		l.emit(itemNull)
+	} else {
+		l.emit(itemIdentifier)
+	}
+	return lexText
+}
+
+func isIdentifierPart(r rune) bool {
+	return (unicode.IsLetter(r) || unicode.IsMark(r) || unicode.IsDigit(r) ||
+		unicode.IsPunct(r)) && !strings.ContainsRune("{}[]:,", r)
 }
 
 func lexWhitespace(l *lexer) stateFn {
@@ -259,9 +391,6 @@ func lexBlockComment(l *lexer) stateFn {
 		if l.next() == eof {
 			break
 		}
-	}
-	if l.pos > l.start {
-		l.emit(itemText)
 	}
 	l.emit(itemEOF)
 	return nil
